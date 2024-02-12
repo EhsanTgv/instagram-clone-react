f201654b871ef38c58e92be3b9af5e322817d350
EhsanTgv <EhsanTgv@gmail.com>
2024-02-12 09:42:47 +0300

    Custom 404 page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home"
+import NoMatch from './Pages/404Page/404Page';
 
-const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route exactl path='/' element={Login} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLog: false
+  }
+  render() {
+    const { isLog } = this.state;
+    return (
+      <div>
+        <Routes>
+          <Route exact path='/' render={() => !isLog ? <Login isLogin={this.hanleLogin} /> : <Home />} />
+          <Route path='*' element={NoMatch} />
+        </Routes>
+      </div>
+    )
+  }
 }
 
 export default App;
